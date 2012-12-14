aea979d10975965a8e2d109657b5922557ea3749
Alejandro Romero <alejo8591@gmail.com>
2012-12-14 17:01:32 -0500

    update model with franchisee codes

--- users/models.py
+++ users/models.py
@@ -6,36 +6,7 @@ from datetime import date, datetime
 from django.db.models.signals import post_save
 
 from datetime import datetime
-"""
-class Entity(models.Model):
-    #Generic Model for User and Group
-    last_update = models.DateTimeField(auto_now=True)
-    
-    def getEntity(self):
-        try:
-            return self.group
-        except:
-            try:
-                return self.user
-            except:
-                return None
-    def isUser(self):
-        try:
-            return self.user is not None
-        except:
-            return False
-        
-    def __unicode__(self):
-        try:
-            return self.getEntity().__unicode__()
-        except:
-            return unicode(self.id)
-    def get_absolute_url(self):
-        try:
-            return self.getEntity().__unicode__()
-        except:
-            return ''
-"""              
+
 class UserProfile(models.Model):
     CITY = (
         ('Bogotá','Bogotá'),
@@ -53,7 +24,7 @@ class UserProfile(models.Model):
     mobile       = models.CharField(max_length=60, help_text="Número de Telefono móvil de contacto", verbose_name="Telefono móvil")
     alternativePhone = models.CharField(max_length=60, help_text="Número de Telefono o FAX", verbose_name="Telefono/FAX")
     address      = models.CharField(max_length=60, help_text="Dirección de Residencia", verbose_name="Dirección")
-    lastAccess   = models.DateTimeField(default=datetime.now, )
+    lastAccess   = models.DateTimeField(default=datetime.now, editable=False)
     
     class Meta:
         ordering = ['firstName']
@@ -68,3 +39,16 @@ def createUser(sender, instance, created, **kwargs):
            profile.save
             
 post_save.connect(createUser, sender=djangoauth.User)
+
+# Model building codes to be used by franchisee 
+class UserFranchiseeCode(models.Model):
+    # partnership with the franchisee or franchisee referenced
+    userFranchisee           = models.ForeignKey(UserProfile, help_text="Códigos del Franquiciado")
+    # code is 4 initially four numbers for users franchisees referenced
+    userFranchiseeCode       = models.CharField(max_length=20);
+    # flag of the use code: True = code used and False = Not used
+    userFranchiseeCodeFlag   = models.BooleanField(default=False, editable=False)
+    # Creation Date
+    userFranchiseeDateCreate = models.DateTimeField(default=datetime.now, editable=False)
+    # Use the code date
+    userFranchiseeDateUse    = models.DateTimeField(editable=False)
