3e3bdf6af1e1d27a2c7b64904a0110dbb41d5a71
Alejandro Romero <alejo8591@gmail.com>
2012-12-14 17:01:53 -0500

    update views for users

--- users/views.py
+++ users/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+
+def new_user(request):
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = UserCreationForm()
+    return render_to_response('./users/newuser.html', {'form':form}, context_instance=RequestContext(request))
