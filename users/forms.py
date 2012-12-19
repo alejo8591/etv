fa4e62a8a9a2a0cc2d1448dcbfff3d5ec8905b68
Alejandro Romero <alejo8591@gmail.com>
2012-12-19 17:12:15 -0500

    updated to have functional ajax with dajax and dajaxice

--- users/forms.py
+++ users/forms.py
@@ -4,25 +4,26 @@ from django.core import validators
 from django.contrib.auth.models import User
 
 class RegistrationForm(forms.Form):
-    
+    # Fields for the creation of the user who is not registered
     identification = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Usuario en el Sistema', 'class':'span3'}))
     email          = forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'email', 'class':'span3'}))
     passwordOne    = forms.CharField(widget=forms.PasswordInput(render_value=False, attrs={'placeholder':'contraseña base', 'class':'span3'}))
     passwordTwo    = forms.CharField(widget=forms.PasswordInput(render_value=False, attrs={'placeholder':'Repetir contraseña base', 'class':'span3'}))
     franchiseeCode = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Código Franquiciado', 'class':'span3'}))
-"""       
-        def isValidUsername(self, field_data,all_data):
-            try:
-                User.objects.get(username=field_data)
-            except User.DoesNotExist:
-                return    
-            raise validators.ValidationError("The Username '%s' is alredy taken.") %(field_data)
+    
+    #Method for verifying that the user is not in the database
+    def isValidFranchesee(franchisee):
+        try:
+            User.objects.get(username=franchisee)
+        except User.DoesNotExist:
+            return True
+        return False
         
-        def save(self, new_data):
-            u = User.objects.create_user(new_data['username'],
-                                        new_data['email'],
-                                        new_data['password1'])
-            u.is_active = False
-            u.save()
-            return u
-"""
+    # Method for creating the user in the database
+    def save(self, new_data):
+        u = User.objects.create_user(new_data['identification'],
+                                    new_data['email'],
+                                    new_data['password1'])
+        u.is_active = False
+        u.save()
+        return u
