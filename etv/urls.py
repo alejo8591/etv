196b63b75ebca45c0c1267aaffe48bd1edbed6cc
Alejandro Romero <alejo8591@gmail.com>
2012-12-18 22:57:07 -0500

    update urls from register user

--- etv/urls.py
+++ etv/urls.py
@@ -1,8 +1,21 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from etv import settings
+#from users.views import register
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
        url(r'^admin/', include(admin.site.urls)),
        (r'^grappelli/', include('grappelli.urls')),
+       url(r'^register/', 'users.views.register'),
 )
+if settings.DEBUG:
+    # Serve static files
+    urlpatterns += staticfiles_urlpatterns()
+    # Serve uploaded files
+    urlpatterns += patterns('',
+        url(r'^uploads/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
+        )
