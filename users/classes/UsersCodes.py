6eb77d51d66daf483d58f66acbf5b5ef685a6c8f
Alejandro Romero <alejo8591@gmail.com>
2012-12-29 00:03:16 -0500

    update with new structure

--- users/classes/UsersCodes.py
+++ users/classes/UsersCodes.py
@@ -3,21 +3,27 @@ this algorithm is created by Alejandro Romero @alejo8591 MIT or BSD licensed
 """
 from random import randint
 
-class UserCode:
-    def __init__(self):
-        return
-    
-    def generateCodes(self, identification):
-        i=0
-        alphabet = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-        numeric  = ('1','2','3','4','5','5','7','8','9','0')
-        alphanumeric = (alphabet, numeric)
-        codes,position = [], 0
-        for i in  range(20):
-            j,code=0,''
-            for j in range(8):
-                option = randint(0,1)
-                if option == 0: code += alphanumeric[option][randint(0,25)]
-                else:code += alphanumeric[option][randint(0,9)]
-            codes.append(code)
-        return codes
+class UserCode(object):
+    """
+        Instantiate the class with the following parameters:
+        :param quantity: Number of codes to return
+        :param lonitude: Catidad character must have a code
+    """
+    def __init__(self, quantity, longitude):
+        self._quantity = quantity
+        self._longitude = longitude
+        
+    def generateCodes(self):
+        self.i=0
+        self.alphabet = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+        self.numeric  = ('1','2','3','4','5','5','7','8','9','0')
+        self.alphanumeric = (self.alphabet, self.numeric)
+        self.codes,self.position = [], 0
+        for self.i in  range(self._quantity):
+            self.j,self.code=0,''
+            for self.j in range(self._longitude):
+                self.option = randint(0,1)
+                if self.option == 0: self.code += self.alphanumeric[self.option][randint(0,25)]
+                else:self.code += self.alphanumeric[self.option][randint(0,9)]
+            self.codes.append(self.code)
+        return self.codes
