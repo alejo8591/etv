c25b581f3c8360864e80facb3cd7fb6e27d441e5
Alejandro Romero <alejo8591@gmail.com>
2013-01-09 11:44:41 -0500

    update test for users app

--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.test import LiveServerTestCase, TestCase
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver import
 
 class AdminTest(TestCase):
      
@@ -11,10 +12,34 @@ class AdminTest(TestCase):
         
     def test_can_admin(self):
         self.browser.get('http://127.0.0.1:8000' + '/register/')
+           
+        self.assertIn("register", self.browser.title)
         
-        self.assertIn("registe", self.browser.title)
+        user = self.browser.find_element_by_id("id_identification")
+        user.send_keys("80912070")
+        user.send_keys(Keys.RETURN)
         
-        #self.fail('finish this error for admin site')
+        email = self.browser.find_element_by_id("id_email")
+        email.send_keys("alejo8591@gmail.com")
+        email.send_keys(Keys.RETURN)
+        
+        passwordOne = self.browser.find_element_by_id("id_passwordOne")
+        passwordOne.send_keys("80912070")
+        passwordOne.send_keys(Keys.RETURN)
+        
+        passwordTwo = self.browser.find_element_by_id("id_passwordTwo")
+        passwordTwo.send_keys("80912070")
+        passwordTwo.send_keys(Keys.RETURN)
+        
+        franchiseeCode = self.browser.find_element_by_id("id_franchiseeCode")
+        franchiseeCode.send_keys("80912070")
+        franchiseeCode.send_keys(Keys.RETURN)
+        
+        buttonSend = self.browser.find_elements_by_id("buttonSend")
+        
+        #buttonSend.submit()
+        
         
     def tearDown(self):
+        self.browser.close()
         self.browser.quit()
