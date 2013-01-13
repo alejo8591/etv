81c564f56f6dd05fe1a4194143a71a7e1ba14eca
Alejandro Romero <alejo8591@gmail.com>
2013-01-13 02:35:11 -0500

    Disabled / verified / Return Registration form options Franchisee

--- users/ajax.py
+++ users/ajax.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from dajax.core import Dajax
 from dajaxice.utils import deserialize_form
 from dajaxice.decorators import dajaxice_register
@@ -17,7 +18,6 @@ from users.classes.UserRegistration import UserRegistration
 """
 
 @dajaxice_register(method='POST')
-
 def send_form(request,form):
     dajax = Dajax()
     
@@ -32,38 +32,56 @@ def send_form(request,form):
             data = formf.cleanData()
             # validation data: Franchisee code, email, Franchisee
             validFranchisee = formf.validDataFranchisee()
+            print validFranchisee
             #checking fields not in use
-            if (validFranchisee['franchisee'] == True and validFranchisee['email'] == True and validFranchisee['franchiseeCode']['flag'] == True and validFranchisee['franchiseeCode']['id'] > 0 or validFranchisee['franchiseeCode']['flag'] == False and validFranchisee['franchiseeCode']['id'] == 0):
+            if (validFranchisee['franchisee'] == True and validFranchisee['email'] == True and validFranchisee['franchiseeCode']['flag'] == True and validFranchisee['franchiseeCode']['id'] >= 0 ):
                 
                 formf.saveUser(); formf.activationKey(); formf.newUserProfile()
                    
                  # change in the respective fields successful color
                 for field in form.fields:
+                    # removing the entry fields
+                    dajax.remove('#id_%s' % field)
+                    # adding fields not editable
+                    dajax.append('#f_%s' % field, 'innerHTML','<span id="id_%s" class="input-large uneditable-input">No puede Modificar</span>' % field)
                     dajax.remove_css_class('#label_%s' % field, 'label label-important')
                     dajax.add_css_class('#label_%s' % field, 'label label-success')
                     dajax.remove_css_class('#f_%s' % field, 'control-group error')
                     dajax.add_css_class('#f_%s' % field, 'control-group success')
                     dajax.remove('#label_%s' % field)
-                    dajax.append('#f_%s'% field, 'innerHTML','<span id="label_%s" class="label label-success">Correcto</span>'% field)
+                    dajax.append('#f_%s'% field, 'innerHTML','<span id="label_%s" class="label label-success">Correcto</span>' % field)
                 
                 # Change other fields with successful Color
                 dajax.remove_css_class('#errors','alert alert-info')
                 dajax.remove_css_class('#errors','alert alert-error')
                 dajax.add_css_class('#errors','alert alert-success')
-                dajax.assign('#errors', 'innerHTML', '<b>Muchas Gracias se Registro con exito!</b>')
+                # adding button unmodifiable
+                dajax.remove('#buttonSend')
+                dajax.append('#my_form', 'innerHTML', '<a class="btn btn-primary disabled">Resgistrarme!</a>')
+                dajax.add_css_class('#buttonSend', 'btn btn-primary disabled')
+                
+                dajax.assign('#errors', 'innerHTML', '<b>Muchas Gracias te Registraste con exito!</b>')
+                
+                message_correct = """
+                                
+                                  """
                 dajax.assign('#modalBody', 'innerHTML', '<h5>Para tener en cuenta:</h5><span class="badge badge-inverse">1</span> Revisa tu correo electronico <span class="label label-info">%s</span>' % data['email'])
                 # Modal with info subcriptor
                 dajax.script("$('#myModal').modal('show')")
-                
-                # Send message
+                # send email
                 formf.send_mails('alejo8591@gmail.com')
                         
             # If the user or user code already exists or use   
             else:
-                dajax.assign('#modalBody', 'innerHTML', '<h5>Algo salio mal:</h5><span class="badge badge-inverse">1</span> Revisa tu correo electronico <span class="label label-info">%s</span>' % data['email'])
-                # Announcement correct entry
-                #dajax.script('$("#modal-body").append(function(){<h5>Para tener en cuenta:</h5><span class="badge badge-inverse">1</span> Revisa tu correo electronico <span class="label label-info">%s</span>});' % data['email'])
-                #<span class="badge badge-inverse">2</span> Verifica el correo enviado por <span class="label label-info">alejo8591@gmail.com</span><span class="badge badge-inverse">3</span> por ultimo click en el enlace de <span class="label label-important">Finalizar Registro</span>
+                message_errors = """<h5><i class="icon-warning-sign"></i> Verificar los siguientes <span class="label label-important">ERRORES</span> en tu Registro como Franquiciado:</h5><br />
+                                    <span class="badge badge-important">1</span> <b>La identificación o Documento ya existe</b><br />
+                                    <span class="badge badge-important">2</span> <b>Cuenta de correo electrónico ya existe</b><br />
+                                    <span class="badge badge-important">3</span> <b>Las contraseñas no concuerdan</b><br />
+                                    <span class="badge badge-important">4</span> <b>El Código de referencia ya esta en uso</b><br />
+                                    <span class="badge badge-important">5</span> <b>El Código de referencia indicado No existe</b>
+                                     
+                                 """
+                dajax.assign('#modalBody', 'innerHTML', message_errors)
                 dajax.script("$('#myModal').modal('show')")  
         else:
             dajax.remove_css_class('#errors','alert-info')
@@ -75,7 +93,8 @@ def send_form(request,form):
                 dajax.add_css_class('#f_%s' % error, 'control-group error')
                 dajax.remove('#label_%s'% error)
                 dajax.append('#f_%s' % error, 'innerHTML', '<span id="label_%s" class="label label-important">Corregir este Campo</span>'% error)
-    
+        
         return dajax.json()
+    
     else:
         return dajax.json()
