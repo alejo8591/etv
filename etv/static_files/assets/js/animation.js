9ade58427d0881636f908e1938bc44275b1df124
Alejandro Romero <alejo8591@gmail.com>
2013-01-22 01:11:29 -0500

    update with modified templates

--- etv/static_files/assets/js/animation.js
+++ etv/static_files/assets/js/animation.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function(){	
 	$('#buttonSend').click(function(event){
-	    Dajaxice.users.send_form(Dajax.process, {'form':$('#my_form').serialize(true)});		   
-    });
+		Dajaxice.users.send_form(Dajax.process, {'form':$('#my_form').serialize(true)});
+	});
 });
